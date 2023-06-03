adc82dc281cc58c406411f1bf127ec993a3c41fc
Kunal <futuremaker1001@gmail.com>
2023-06-03 18:29:26 +0530

    cards|modals|complete front-end

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,39 +7,50 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  public data="hello yo!"; 
-  public color = "text-primary"
-  public con= false;
-  public pd = [
-    {
-      name: "Sq",
-      id:2,
-    },
-    {
-      name: "Sw",
-      id:3,
-    },
-    {
-      name: "Se",
-      id:4,
-    },
-    {
-      name: "Sr",
-      id:1,
-    },
-    {
-      name: "St",
-      id:5,
-    },
-  ]
-  public constructor(){
-    this.f1()
-  }
-  public f1(){
-    alert("f1")
-  }
-  public f2(){
-    alert("f2")
+  public productDetail:any;
+
+  public getProductDetail(product:any){
+
+    console.log(product);
+    this.productDetail= product;
+
   }
 
+  public products = [
+    {
+      id:1,
+      name:'jacket',
+      price: '₹120000',
+      description:'A beautiful Jacket in leather, by BuyIt.com. This premium jacket is not for soft hearted.',
+      url:'../../../assets/images/home/placeimg_640_480_any.jpg',
+    },
+    {
+      id:2,
+      name:'jacket',
+      price: '₹1200000',
+      description:'A beautiful Jacket in leather, by BuyIt.com. This premium jacket is not for soft hearted.',
+      url:'../../../assets/images/home/placeimg_640_480_any.jpg',
+    },
+    {
+      id:3,
+      name:'jacket',
+      price: '₹1200001',
+      description:'A beautiful Jacket in leather, by BuyIt.com. This premium jacket is not for soft hearted.',
+      url:'../../../assets/images/home/placeimg_640_480_any.jpg',
+    },
+    {
+      id:4,
+      name:'jacket',
+      price: '₹1200002',
+      description:'A beautiful Jacket in leather, by BuyIt.com. This premium jacket is not for soft hearted.',
+      url:'../../../assets/images/home/placeimg_640_480_any.jpg',
+    },
+    {
+      id:5,
+      name:'jacket',
+      price: '₹1200003',
+      description:'A beautiful Jacket in leather, by BuyIt.com. This premium jacket is not for soft hearted.',
+      url:'../../../assets/images/home/placeimg_640_480_any.jpg',
+    },
+  ]
 }
